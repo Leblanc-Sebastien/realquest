4eeacaf09292f647a0c039b0cb9e14737b832f56
Leblanc Sébastien <sugii.noob@gmail.com>
2025-05-17 10:00:32 +0200

    Initial commit - setup Nuxt + Capacitor + Prisma

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.realquest.app',
+  appName: 'realquest',
+  webDir: '.output/public',
+};
+
+export default config;
