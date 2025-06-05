0dabee1fd09661ace3ef5c1f37ae5621afbbd992
Leblanc Sébastien <sugii.noob@gmail.com>
2025-06-05 12:00:40 +0200

    feat: error page created

---
+++ error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen bg-surface text-text text-center px-4"
+  >
+    <img
+      src="/logos/realquest_logo.png"
+      alt="RealQuest Logo"
+      class="w-32 h-32 mb-6"
+    />
+    <h1 class="text-4xl font-bold mb-4">Oups...</h1>
+    <p class="text-lg mb-6">Vous vous êtes trompé de quête !</p>
+    <NuxtLink
+      @click="handleError"
+      class="bg-success text-white px-6 py-2 rounded hover:bg-success/90 transition"
+    >
+      Revenir à l'accueil
+    </NuxtLink>
+  </div>
+</template>
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+const handleError = () => clearError({ redirect: '/' });
+</script>
